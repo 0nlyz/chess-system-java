0e974bdf5eca871f48a0bbb21afb0740330849f7
Caio Trigolo <trigolo20@gmail.com>
2023-09-11 08:49:10 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessExcepiton.java
@@ -0,0 +1,10 @@
+package chess;
+
+public class ChessExcepiton extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ChessExcepiton(String msg) {
+		super(msg);
+	}
+}
