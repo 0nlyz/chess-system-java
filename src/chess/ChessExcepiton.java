30e93f591a8b5554385e7dc795042d8780cada9e
Caio Trigolo <trigolo20@gmail.com>
2023-09-11 09:26:03 -0300

    Moving pieces

--- src/chess/ChessExcepiton.java
+++ src/chess/ChessExcepiton.java
@@ -1,6 +1,8 @@
 package chess;
 
-public class ChessExcepiton extends RuntimeException {
+import boardgame.BoardException;
+
+public class ChessExcepiton extends BoardException {
 
 	private static final long serialVersionUID = 1L;
 	
